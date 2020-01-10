92fc579c1f2e975dc7cb52e67bede3b1f45e3b54
Aija12345 <54476940+Aija12345@users.noreply.github.com>
2020-01-10 17:04:10 +0200

    skaists

--- static/chats.js
+++ static/chats.js
@@ -1,75 +1,84 @@
-const ATJAUNOT = 500;
-
+cconst ATJAUNOT = 500;
+const VERSIJA = "0.4"
 
 /*
-pē 5000 milisekundeēm atjaunosies
 Ielādē tērzēšanas datus no servera
 Uzstāda laiku pēc kāda atkārtoti izsaukt šo pašu funkciju
-await - sagaida kad serveris atbild
-atbilse būs kā js datu objekts
-izsauc funkciju ar raadi chatu vienkarsi
-async ir kopā tika iar await
 */
 async function lasiChatu() {
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
     await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
     await lasiChatu();
-  
-    
 }
 
 
 function raadiChatuVienkarsi(dati) {
-    const jaunaRinda = "<br/>";
-    let chats = "";
-    let chataDiv = document.getElementById("chats");
-    for (let rinda of dati["chats"]) {
+    var jaunaRinda = "<br/>";
+    var chats = "";
+    var chataDiv = document.getElementById("chats");
+    for (var rinda of dati["chats"]) {
         chats = chats + rinda + jaunaRinda;
     }
     chataDiv.innerHTML = chats;
 }
 
 
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
+
+
 /*
-chataDiv.innerHTML = chats; - ieliek htmlā
-Augša izveidota čata vieta, kur tiek ielikta inf katrā rindāS
 Publicē tērzēšanas ziņas datus uz serveri
 */
 async function suutiZinju() {
     // Nolasa ievades lauka saturu
-
     let zinjasElements = document.getElementById("zinja");
-    let zinja = zinjasElements.value;
-    // izdzēš ievades lauku
-    zinjasElements.value = "";
+    const zinja = zinjasElements.value;
 
-    const atbilde = await fetch('/chats/suuti', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "chats": zinja })
-    });
-    const datuObjekts = await atbilde.json();
-    
-    
-    // Metode Post - kā veikt pieprasījumu un kādā veidā sūtīsim
-    // parāda jauno chata saturu  
-    //kas ir fetch??  - vēršas pie chats 
-   
-    raadiChatuVienkarsi(datuObjekts);
+    // pārbaudām vai ir vispār kaut kas ierakstīts
+    if (zinja.length > 0) {
+
+        // izdzēš ievades lauku
+        zinjasElements.value = "";
+
+        const atbilde = await fetch('/chats/suuti', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "chats": zinja })
+        });
+        const datuObjekts = await atbilde.json();
+        // parāda jauno chata saturu
+        raadiChataRindas(datuObjekts);
+    } else {
+        console.log("Tukšu ziņu nesūtām uz serveri")
+    }
+
 }
-
-
-// Ērtības funkcionalitāte
-// Atrod ievades lauku
-var ievadesLauks = document.getElementById("zinja");
-// Gaida signālu no klaviatūras, ka ir nospiests Enter taustiņš
-ievadesLauks.addEventListener("keyup", function(event) {
-  // Numur 13 ir "Enter" taustiņš
-  if (event.keyCode === 13) {
-    suutiZinju();
-  }
-});
